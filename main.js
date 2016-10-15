9db507be5bb6b0d5f13eca793d284bd4fa1f27f6
Jason Green <jgreen2@uoregon.edu>
2016-10-15 10:45:31 -0700

    Push Home Team Name to AWS with NodeJs

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ const Joi = require('joi');
 const AWS = require('aws-sdk');
 const SNS = new AWS.SNS({region: 'us-west-2'});
 
+AWS.config.loadFromPath('./config.json');
+console.log(AWS.config.credentials.get())
+
 const iosAppArn = "arn:aws:sns:us-west-2:164008979560:app/APNS_SANDBOX/QuackCon2016"
 const andrAppArn = ""
 const errorStrings = {
 	missingParam: 'MISSING_REQUIRED_PARAMETER',
-	unknownTopic: 'UNKNOWN_TOPIC'
+	unknownTopic: 'UNKNOWN_TOPIC',
+	unknownType: 'UNKNOWN_DEVICE_TYPE'
 }
 
 console.log(SNS.endpoint);
@@ -98,6 +102,8 @@ server.route({
 				error: errorStrings.missingParam,
 				info: missing
 			}
+			reply(error);
+			return;
 		}
 
 		let appArn;
@@ -106,6 +112,14 @@ server.route({
 		}
 		else if (query.type === 'android') {
 			appArn = andrAppArn;
+		}
+		else {
+			const error = {
+				error: errorStrings.unknownType,
+				info: [query.type]
+			};
+			reply(error);
+			return;
 		}
 
 		const params = {
@@ -160,6 +174,46 @@ server.route({
 					displayName: displayName
 				}
 				reply('Topic successfully created');
+			}
+		});
+	}
+});
+
+server.route({
+	method: 'DELETE',
+	path: '/remove',
+	handler: function(request, reply) {
+		const query = request.query;
+		if (!query.topic) {
+			var missing = []
+			if (!query.topic) missing.push('topic');
+			
+			const error = {
+				error: errorStrings.missingParam,
+				info: missing
+			};
+			reply(error);
+			return;
+		}
+		if (!topics[query.topic]) {
+			const error = {
+				error: errorStrings.unknownTopic,
+				info: [query.topic]
+			}
+			reply(error)
+			return;
+		}
+
+		const params = { TopicArn: topics[query.topic].topicArn };
+		SNS.deleteTopic(params, function(err, data) {
+			if (err) {
+				console.log(err, err.stack);
+				reply('Error deleting topic');
+			}
+			else {
+				console.log(data);
+				delete topics[query.topic];
+				reply('Topic successfully deleted');
 			}
 		});
 	}
@@ -232,7 +286,6 @@ server.start((err) => {
 });
 
 
-
 var http = require("http");
 var url = "http://api.sportradar.us/nfl-ot1/games/c8dc876a-099e-4e95-93dc-0eb143c6954f/boxscore.json?api_key=6vgqj9xr6fqj2es2umvwcc35";
 
@@ -255,12 +308,17 @@ var request = http.get(url, function (response) {
        // console.log(buffer);
         console.log("\n");
        var data = JSON.parse(buffer);
-        //summary = data.season.year[0];
+       var teams = data.summary
 
         // extract the distance and time
-        console.log(data.summary)
-        console.log("Home Team: " + data.summary.home.name);
+        //console.log(data.summary)
+       var hometeam = ("Home Team: " + teams.home.name);
+       var awayteam = ("Away Team: " + teams.away.name);
+       var venue = ("Venue: " + teams.venue.name);
+
+       publish(hometeam, "arn:aws:sns:us-west-2:164008979560:HapiTest")
+       //publish(awayteam, "arn:aws:sns:us-west-2:164008979560:HapiTest")
+      // publish(venue, "arn:aws:sns:us-west-2:164008979560:HapiTest")
         
     }); 
-}); 
-
+}); 		
